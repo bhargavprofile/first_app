e8cbad4e69291741c8fa3490cd2651b96e8b5ec9
tech-bhargava <techbhargava@gmail.com>
2023-09-26 08:34:00 +0530

    work in progress

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 import reviews from './data';
 
 function App() {
-  const [reviews, setReviews] = useState(reviews);
- 
-
+  const [r, setR] = useState(reviews);
+  
   return (
     <div>
-       <h1 style={{textAlign: 'center', letterSpacing: '1px', borderBottom: '4px solid #49a6e9', width: 'max-content', margin: '0 auto'}}>Edupoly Reviews</h1>
+       <h1 style={{textAlign: 'center', letterSpacing: '1px', borderBottom: '4px solid #49a6e9', width: 'max-content', margin: '0 auto', marginBottom: '50px'}}>Edupoly Reviews</h1>
        {
-        reviews.map((a,i)=>{
-          return <div>
+        r.map((a,i)=>{
+          return <div style={{display: 'flex', justifyContent:'center'}}>
                     <div className='card'>
                        <img src={a.image} alt='img'/>
                        <h5>{a.name}</h5>
                        <h4>{a.job}</h4>
                        <p>{a.text}</p>
+
                     </div>
                </div>
         })
