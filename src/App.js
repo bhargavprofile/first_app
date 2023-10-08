40c8405806256e94994039ff94c59c539724b20a
tech-bhargava <techbhargava@gmail.com>
2023-10-08 09:17:34 +0530

    poke

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { Provider } from 'react-redux';
 import './App.css';
-import StudentForm from './StudentForm';
+import { store } from './store';
 
 function App() {
   return (
+    <Provider store={store}>
     <div style={{padding:'10px',border:'2px solid #000'}}>
-       <h1 style={{margin: '0px'}}>App</h1>
-       <StudentForm/>
+       {/* <h1 style={{margin: '0px'}}>App</h1> */}
     </div>
+    </Provider>
   );
 }
 
