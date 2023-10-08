2eecc18bbf88b23c4e1e2366f5b50c7ab75feca7
tech-bhargava <techbhargava@gmail.com>
2023-10-08 12:18:41 +0530

    ws

--- src/features/pokemon/Pokemon.jsx
+++ src/features/pokemon/Pokemon.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { useGetPokemonByNameQuery } from "../../services/pokemon/pokemon";
 
 const Pokemon = () => {
-  const { data, isLoading } = useGetPokemonByNameQuery();
+  const { data, isLoading } = useGetPokemonByNameQuery([]);
   console.log(data);
+  data &&
+    data.map((a) => {
+      console.log(a);
+    });
   return (
     <div>
       <h1>Pokemon</h1>
+      <ul></ul>
+
       <div></div>
     </div>
   );
