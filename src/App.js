57bf09a39a06ce0055e50abd47bd5b6e90bc4632
tech-bhargava <techbhargava@gmail.com>
2023-10-12 11:42:59 +0530

    ttjkghk

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div style={{padding:'10px',border:'2px solid #000'}}>
-       <h1 style={{margin: '0px'}}>App</h1>
+    <div>
+       
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>
+        Patient<span>Care</span>
+      </h1>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Features</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
